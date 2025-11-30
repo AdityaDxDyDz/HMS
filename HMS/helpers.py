0f9cdb4242ed5e165605799a5bd7a5bf9f60a8a7
AdityaDxDyDz <154826572+AdityaDxDyDz@users.noreply.github.com>
2025-12-01 03:49:43 +0530

    Add files via upload

---
+++ HMS/helpers.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from flask import flash, redirect, url_for
+from flask_login import current_user
+
+def roles_required(*roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if current_user.role not in roles:
+                flash("Unauthorized access!", "danger")
+                return redirect(url_for('main.index'))
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
